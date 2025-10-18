19573396a99923c1e7ed5a270f2000712eb7b882
wWinklepicker <sarahdove2005@gmail.com>
2025-10-18 16:43:43 +0200

    Pickups work, and inventory completed

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,15 +1,22 @@
+using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    [SerializeField] Image[] objects;
+    [SerializeField] GameObject[] objects;
     [SerializeField] MousePointer mouse;
     Item[] items = new Item[8];
 
     private void Start()
     {
         EventHandler.Instance.ItemRemovedEvent += RemoveItem;
+        EventHandler.Instance.ItemAddedEvent += AddItem;
+        for (int i = 0; i < objects.Length; i++) 
+        {
+            objects[i].SetActive(false);
+        }
+
     }
     public void SelectItem(int Index)
     {
@@ -20,8 +27,13 @@ public class Inventory : MonoBehaviour
     {
         for (int i = 0; i < items.Length; i++)
         {
-            if (items[i] == null) items[i] = newItem;
-            objects[i].sprite = newItem.Icon;
+            if (items[i] == null)
+            {
+                items[i] = newItem;
+                objects[i].SetActive(true);
+                objects[i].GetComponent<Image>().sprite = newItem.Icon;
+                return;
+            }
         }
     }
 
@@ -29,8 +41,12 @@ public class Inventory : MonoBehaviour
     {
         for (int i = 0; i < items.Length; i++)
         {
-            if (items[i] == removeItem) items[i] = null;
-            objects[i].sprite = null;
+            if (items[i] == removeItem)
+            {
+                items[i] = null;
+                objects[i].SetActive(false);
+                return;
+            }
         }
     }
 }
