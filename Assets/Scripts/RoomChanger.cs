f349852bb939b0473b4dd16058d5eccce470b715
wWinklepicker <sarahdove2005@gmail.com>
2025-10-19 16:27:29 +0200

    door sfx

--- Assets/Scripts/RoomChanger.cs
+++ Assets/Scripts/RoomChanger.cs
@@ -14,6 +14,7 @@ public class RoomChanger : MonoBehaviour
         {
             EventHandler.Instance.FromDungeon = false;
         }
+        SFX.DoorOpening.Play();
         EventHandler.Instance.ChangeRoomEvent?.Invoke(ID);
     }
 
